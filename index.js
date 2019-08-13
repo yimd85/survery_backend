0c2a7119c02ea2f222ec1981a27c82d827864422
yimd85 <yimd85@gmail.com>
2019-08-12 22:30:37 -0400

    services and routes folder added

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
+// const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ', accessToken,  "refreshToken", refreshToken, "profile", profile);
-    }
-  )
-);
+// authRoutes(app);
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
+// same as const authRoutes = require("./routes/authRoutes"); authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
