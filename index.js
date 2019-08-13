b63ec60b891552f083186c6126c04d3ccab378bb
yimd85 <yimd85@gmail.com>
2019-08-13 19:20:00 -0400

    Added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 // const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
