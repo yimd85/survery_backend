0ab049d442f87cde75414de7e2fee1fc68240bf3
David Yim <yimd85@gmail.com>
2019-08-31 23:27:27 -0400

    deserial user

--- services/passport.js
+++ services/passport.js
@@ -9,6 +9,11 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+  done(null, id);
+});
+
+
 passport.use(
   new GoogleStrategy(
     {
